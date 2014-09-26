fb36b7fec915b61db5ed0163f36046e50e5cd7d7
jseban <sebanjulien@gmail.com>
2014-09-26 18:19:20 +0200

    Update MediaType.php
    mise en place du choix des champs par defaut

--- Form/MediaType.php
+++ Form/MediaType.php
@@ -9,45 +9,50 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class MediaType extends AbstractType
-{
+class MediaType extends AbstractType {
 
-
-	public function setDefaultOptions(OptionsResolverInterface $resolver)
-	{
-    	$resolver->setDefaults(array(
-    	    'data_class' => 'Majes\MediaBundle\Entity\Media',
-    	    'csrf_protection' => false,
-    	));
-	}
-
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-
-        $builder->add('type', 'choice', array(
-            'choices' => array(
-                'picture' => 'Picture',
-                'video' => 'Video',
-                'embed' => 'Embed',
-                'document' => 'Document'
-                ),
-            'required' => true));
-
-        $builder->add('folder', 'text', array(
-            'required' => true));
+    public function buildForm(FormBuilderInterface $builder, array $options) {
 
         $builder->add('file', 'file', array('required' => false, 'media_id' => 'id'));
-        $builder->add('embedded', 'textarea', array('required' => false, 'label' => 'If "Embed" type selected'));
-        
-        $builder->add('title', 'text', array('required' => false));
-        $builder->add('author', 'text', array('required' => false));
-        $builder->add('is_protected', 'checkbox', array('required' => false));
 
+        if ($options['title'])
+            $builder->add('title', 'text', array('required' => false));
 
+        if ($options['type']){
+            $builder->add('type', 'choice', array(
+                'choices' => array(
+                    'picture' => 'Picture',
+                    'video' => 'Video',
+                    'embed' => 'Embed',
+                    'document' => 'Document'
+                    ),
+                'required' => true));
+        }
+
+        if ($options['folder'])
+            $builder->add('folder', 'text', array('required' => true));
+        if ($options['embedded'])
+            $builder->add('embedded', 'textarea', array('required' => false, 'label' => 'If "Embed" type selected'));
+        if ($options['author'])
+            $builder->add('author', 'text', array('required' => false));
+        if ($options['is_protected'])
+            $builder->add('is_protected', 'checkbox', array('required' => false));
     }
 
-    public function getName()
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class'      => 'Majes\MediaBundle\Entity\Media',
+            'title'           => true,
+            'type'            => true,
+            'folder'          => true,
+            'embedded'        => true,
+            'author'          => true,
+            'is_protected'    => true,
+            'csrf_protection' => false,
+        ));
+    }
+
+    public function getName() {
         return 'media';
     }
 }
